cd1defd2febabf90e981f9abfb472d79af159852
Robert Segal <rob@getsetgames.com>
2016-08-07 23:17:33 -0400

    Android Studio Playhaven test app

---
+++ MyPlayhavenTestApp/androidsdk/src/main/java/com/playhaven/android/Version.java
@@ -0,0 +1,24 @@
+package com.playhaven.android;
+
+/**
+ * Auto-generated file, contains project and version control state info.
+ */
+public final class Version {
+  public static String PLUGIN_BUILD_TIME = "2014-11-24 14:08";
+
+  public static String PROJECT_NAME = "API";
+  public static String PROJECT_GROUP = "com.playhaven.android";
+  public static String PROJECT_ARTIFACT = "playhaven-api";
+  public static String PROJECT_VERSION = "3.0.4";
+  public static String SOURCE_VERSION = "3.0.4-3-g3c016a0";
+  public static String SOURCE_BRANCH = "develop";
+  public static Boolean SOURCE_DIRTY = false;
+  public static String SOURCE_WORKING_COPY = "/Users/jeremyberman/git/PlayHaven/sdk-android-2";
+
+  public static String SERVER_BUILD_PLAN = "N/A";
+  public static String SERVER_BUILD_ID = "N/A";
+  public static String SERVER_BUILD_TIME = "N/A";
+
+  public static String BANNER_FULL = "Build info: API version 3.0.4 built on 2014-11-24 14:08 source version 3.0.4-3-g3c016a0, build id: N/A-N/A.";
+  public static String BANNER = "3.0.4-3.0.4-3-g3c016a0 2014-11-24 14:08 ";
+}
---
+++ MyPlayhavenTestApp/app/src/main/java/com/example/robertsegal/myplayhaventestapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.robertsegal.myplayhaventestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
